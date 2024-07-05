fe2778cfde4adf7c089dba3e7b6b6ab56724748f
partialarts <145933508+jemlawX@users.noreply.github.com>
2024-07-05 15:27:55 +0100

    Added netlify.toml config file

--- src/utils/airtableapi.js
+++ src/utils/airtableapi.js
@@ -1,11 +1,12 @@
 import Airtable from "airtable";
 const apiKey = import.meta.env.VITE_AIRTABLE_API_KEY;
+const baseId = import.meta.env.VITE_AIRTABLE_BASE_ID;
 
 Airtable.configure({
     endpointUrl: 'https://api.airtable.com',
     apiKey: apiKey,
 });
 
-const base = Airtable.base("appCfHeiwqePN27kL");
+const base = Airtable.base(baseId);
 
 export default base;
