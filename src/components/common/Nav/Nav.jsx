6376891982136fc5c0361506cf5ab3797c946a55
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-07-10 21:24:24 +0100

    adding tailwind css

--- src/components/common/Nav/Nav.jsx
+++ src/components/common/Nav/Nav.jsx
@@ -3,27 +3,27 @@ import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 const Nav = () => {
-return (
-    <nav className="nav">
-        <div className="center">
-        <NavLink
-        to="home"
-        >
-        Home
-        </NavLink>
-            <NavLink
-        to="googlesheets"
-        >
-        Google Sheets
-        </NavLink>
-        <NavLink
-        to="airtable"
-        >
-        AirTable
-        </NavLink>
-        </div>
+    return (
+        <nav className="nav">
+            <div className="center">
+                <NavLink
+                    to="home"
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    to="googlesheets"
+                >
+                    Google Sheets
+                </NavLink>
+                <NavLink
+                    to="airtable"
+                >
+                    AirTable
+                </NavLink>
+            </div>
         </nav>
-);
+    );
 };
 
 export default Nav;
