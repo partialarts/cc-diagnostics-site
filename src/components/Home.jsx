4566d747a3c5c6544e9e47e851af98b91d2e41c2
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-07-14 18:43:37 +0100

    adding home page sections, adjusting padding between sections

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Hero from './homeComponents/Hero'
+import Stats from './homeComponents/Stats';
+import MethicaKit from './homeComponents/MethicaKit'
+import Mission from './homeComponents/Mission'
+import HomeNews from './homeComponents/HomeNews';
 
 function Home() {
 
     return (
+        <>
         <Hero />
+        <Stats />
+        <Mission />
+        {/* <MethicaKit /> */}
+        <HomeNews />
+        </>
     )
 
 }
