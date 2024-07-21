506d52ae3a74802b5ec84d8c9b3bad0b60e6563c
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-07-21 11:05:56 +0100

    alighning news text left

--- src/components/News/components/Articles.jsx
+++ src/components/News/components/Articles.jsx
@@ -1,7 +1,7 @@
 const posts = [
     {
       id: 1,
-      title: 'Boost your conversion rate',
+      title: 'News Article 1',
       href: '#',
       description:
         'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
@@ -9,10 +9,10 @@ const posts = [
         'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
       date: 'Mar 16, 2020',
       datetime: '2020-03-16',
-      category: { title: 'Marketing', href: '#' },
+      category: { title: 'Event', href: '#' },
       author: {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
+        name: 'Nutte',
+        role: 'CMO',
         href: '#',
         imageUrl:
           'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -26,9 +26,8 @@ const posts = [
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:max-w-4xl">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-              Learn how to grow your business with our expert advice.
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Latest News</h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600">placeholder
             </p>
             <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
               {posts.map((post) => (
@@ -53,7 +52,7 @@ const posts = [
                         {post.category.title}
                       </a>
                     </div>
-                    <div className="group relative max-w-xl">
+                    <div className="group text-left relative max-w-xl">
                       <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                         <a href={post.href}>
                           <span className="absolute inset-0" />
