19f7a542aa50aff0335e5b91bacbcb9a5e4327a3
partialarts <145933508+jemlawX@users.noreply.github.com>
2024-08-02 19:35:14 +0100

    remove random dash

--- src/components/News/components/LatestNews.jsx
+++ src/components/News/components/LatestNews.jsx
@@ -136,11 +136,11 @@ export default function LatestNews() {
           className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
         />
         <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-      </div>-
+      </div>
       <div className="max-w-xl">
         <div className="group   
  relative">
-          <h3 className="mt-1 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
             <a href={post.fields.href}>   
 
               <span className="absolute inset-0" />
