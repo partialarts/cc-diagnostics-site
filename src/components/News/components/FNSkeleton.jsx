18e66025ec785e639fa4416cc2494563f0788a73
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-08-04 14:25:09 +0100

    adding key to FNSkeleton fixing console error

--- src/components/News/components/FNSkeleton.jsx
+++ src/components/News/components/FNSkeleton.jsx
@@ -7,12 +7,12 @@ export default function FNSkeleton() {
                 </div>
                 <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {[...Array(3)].map((_, index) => (
-                        <div className="flex w-52 flex-col gap-4">
-  <div className="skeleton h-32 w-full"></div>
-  <div className="skeleton h-4 w-28"></div>
-  <div className="skeleton h-4 w-full"></div>
-  <div className="skeleton h-4 w-full"></div>
-</div>
+                        <div key={index} className="flex w-52 flex-col gap-4">
+                            <div className="skeleton h-32 w-full"></div>
+                            <div className="skeleton h-4 w-28"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                        </div>
                     ))}
                 </div>
             </div>
