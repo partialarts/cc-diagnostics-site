32ff37e2bfd28e6e1c23f127527c06e2267f2203
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-08-04 14:18:37 +0100

    styling returned results on document search

--- src/components/SearchDocs/SearchBar.jsx
+++ src/components/SearchDocs/SearchBar.jsx
@@ -1,34 +1,60 @@
+import React, { useState } from "react";
+import { DocumentIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
-import { BarsArrowUpIcon, UsersIcon } from '@heroicons/react/20/solid'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+const SearchBar = () => {
+  const [results, setResults] = useState([]);
 
-export default function SearchBar() {
+  // Mock search function to update results
+  const handleSearch = (event) => {
+    // Mock results, replace this with actual search logic
+    setResults([
+      { id: 1, title: "Product Title" },
+    //   { id: 2, title: "Product Title 2" },
+    //   { id: 3, title: "Product Title 3" },
+    //   { id: 4, title: "Product Title 4" },
+    ]);
+  };
+
   return (
     <div>
-      <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-600">
-        enter your lot number
+      <label htmlFor="lotNo" className="block text-sm font-medium leading-6 text-gray-600">
+        Enter your lot number
       </label>
       <div className="mt-2 flex rounded-md shadow-sm">
         <div className="relative flex flex-grow items-stretch focus-within:z-10">
           <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">L
-            {/* <UsersIcon aria-hidden="true" className="h-5 w-5 text-gray-400" /> */}
+            {/* starting format for the UDI */}
           </div>
           <input
             id="lotNo"
             name="lotNo"
-            type="interger"
+            type="text"
             placeholder="XXXXX"
-            className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-ccDarkBlue sm:text-sm sm:leading-6"
+            onChange={handleSearch}
+            className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-ccDarkBlue sm:text-sm sm:leading-6"
           />
         </div>
         <button
           type="button"
+          onClick={handleSearch}
           className="rounded-none rounded-r-md relative -ml-px inline-flex items-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 hover:bg-gray-50"
         >
           <MagnifyingGlassIcon aria-hidden="true" className="-ml-0.5 h-5 w-5 text-gray-400" />
           Search
         </button>
       </div>
+      <div className="mt-6 w-full flex justify-center">
+        {/* <div className="p-4 shadow-md rounded-md w-full lg:w-auto"> */}
+          {results.map((result) => (
+            <div key={result.id} className="p-4 shadow-md rounded-md w-full lg:w-auto flex items-center gap-x-4 py-2">
+              <DocumentIcon className="h-6 w-6 text-gray-600" />
+              <h5 className="text-lg font-medium text-gray-900">{result.title}</h5>
+            </div>
+          ))}
+        </div>
+      {/* </div> */}
     </div>
-  )
-}
+  );
+};
+
+export default SearchBar;
--- src/components/SearchDocs/SearchCTA.jsx
+++ src/components/SearchDocs/SearchCTA.jsx
@@ -1,19 +1,18 @@
 import SearchBar from "./SearchBar";
 
 export default function SearchCTA() {
-    return (
-      <div className="bg-ccAliceBlue">
-        <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Already purchased?
-            <br />
-            Download your documentation
-          </h2>
-          <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
-           <SearchBar/>
-          </div>
+  return (
+    <div className="bg-ccAliceBlue">
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Already purchased?
+          <br />
+          Download your documentation
+        </h2>
+        <div className="mt-10 flex flex-col items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
+          <SearchBar />
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  );
+}
