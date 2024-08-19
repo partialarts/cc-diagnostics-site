a5a56a367ada418705810d579c2c4b585d0172e5
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-08-19 10:49:47 +0100

    improving spacing on footer between searchbar and p tag

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -25,44 +25,22 @@ const navigation = {
         </svg>
       ),
     },
-    // {
-    //   name: "Instagram",
-    //   href: "#",
-    //   icon: (props) => (
-    //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-    //       <path
-    //         fillRule="evenodd"
-    //         d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-    //         clipRule="evenodd"
-    //       />
-    //     </svg>
-    //   ),
-    // },
-    // {
-    //   name: "X",
-    //   href: "#",
-    //   icon: (props) => (
-    //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-    //       <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.0167L12.871 11.0131L18.3224 18.9303H15.9356L11.5541 13.0956Z" />
-    //     </svg>
-    //   ),
-    // },
     {
       name: "LinkedIn",
       href: "https://www.linkedin.com/company/cc-diagnostics",
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
- <path
-      fillRule="evenodd"
-      d="M20.3292112,2 C21.2519624,2 22,2.74752185 22,3.67078882 L22,20.3292112 C22,21.2519624 21.2524781,22 20.3292112,22 L3.67078882,22 C2.74803764,22 2,21.2524781 2,20.3292112 L2,3.67078882 C2,2.74803764 2.74752185,2 3.67078882,2 L20.3292112,2 Z M15.51875,9.5 C14.0993287,9.5 13.128125,10.127356 12.6956992,10.8562567 L12.625,10.9858333 L12.625,9.625 L9.91666667,9.625 L9.91666667,19.2083333 L12.8333333,19.2083333 L12.8333333,14.56625 C12.8333333,13.0104167 13.40625,12.0208333 14.7833333,12.0208333 C15.7330797,12.0208333 16.1315784,12.8606664 16.1644352,14.3580086 L16.1666667,14.56625 L16.1666667,19.2083333 L19.0833333,19.2083333 L19.0833333,13.9154167 C19.0833333,11.0575 18.3995833,9.5 15.51875,9.5 Z M7.83333333,9.5 L4.91666667,9.5 L4.91666667,19.0833333 L7.83333333,19.0833333 L7.83333333,9.5 Z M6.375,4.5 C5.33958333,4.5 4.5,5.33958333 4.5,6.375 C4.5,7.41041667 5.33958333,8.25 6.375,8.25 C7.41041667,8.25 8.25,7.41041667 8.25,6.375 C8.25,5.33958333 7.41041667,4.5 6.375,4.5 Z"
-      clipRule="evenodd"
-    />
+          <path
+            fillRule="evenodd"
+            d="M20.3292112,2 C21.2519624,2 22,2.74752185 22,3.67078882 L22,20.3292112 C22,21.2519624 21.2524781,22 20.3292112,22 L3.67078882,22 C2.74803764,22 2,21.2524781 2,20.3292112 L2,3.67078882 C2,2.74803764 2.74752185,2 3.67078882,2 L20.3292112,2 Z M15.51875,9.5 C14.0993287,9.5 13.128125,10.127356 12.6956992,10.8562567 L12.625,10.9858333 L12.625,9.625 L9.91666667,9.625 L9.91666667,19.2083333 L12.8333333,19.2083333 L12.8333333,14.56625 C12.8333333,13.0104167 13.40625,12.0208333 14.7833333,12.0208333 C15.7330797,12.0208333 16.1315784,12.8606664 16.1644352,14.3580086 L16.1666667,14.56625 L16.1666667,19.2083333 L19.0833333,19.2083333 L19.0833333,13.9154167 C19.0833333,11.0575 18.3995833,9.5 15.51875,9.5 Z M7.83333333,9.5 L4.91666667,9.5 L4.91666667,19.0833333 L7.83333333,19.0833333 L7.83333333,9.5 Z M6.375,4.5 C5.33958333,4.5 4.5,5.33958333 4.5,6.375 C4.5,7.41041667 5.33958333,8.25 6.375,8.25 C7.41041667,8.25 8.25,7.41041667 8.25,6.375 C8.25,5.33958333 7.41041667,4.5 6.375,4.5 Z"
+            clipRule="evenodd"
+          />
         </svg>
       ),
     },
     {
       name: "YouTube",
-      href: "https://www.youtube.com/channel/your-channel-id",  // Replace with the actual URL
+      href: "https://www.youtube.com/channel/your-channel-id", // Replace with the actual URL
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -73,12 +51,10 @@ const navigation = {
         </svg>
       ),
     },
-    
   ],
 };
 
 const Partners = (partner) => {
-
   return (
     <ul role="list" className="mt-6 space-y-4">
       <li key={partner.id}>
@@ -88,28 +64,22 @@ const Partners = (partner) => {
           rel="noopener noreferrer"
           className="text-sm leading-6 text-gray-600 hover:text-ccLightBlue"
         >
-        <h6>{partner.fields.Partner}</h6>
-          {/* <img src={partner.fields.Logo && partner.fields.Logo.length > 0 ? partner.fields.Logo[0].url : placeholder} alt={partner.fields.Partner} className="h-6" /> */}
+          <h6>{partner.fields.Partner}</h6>
         </a>
       </li>
     </ul>
   );
 };
 
-const contact = (contact) => {
+const ContactInfo = (contact) => {
   return (
     <ul role="list" className="mt-6 space-y-4">
       <li key={contact.id}>
-        <p
-          // href={contact.href}
-          className="text-sm leading-6 text-gray-600"
-        >
-       {contact.fields.info}
-        </p>
+        <p className="text-sm leading-6 text-gray-600">{contact.fields.info}</p>
       </li>
     </ul>
-  )
-}
+  );
+};
 
 export default function Footer() {
   return (
@@ -138,9 +108,11 @@ export default function Footer() {
                 </a>
               ))}
             </div>
-            <div className="text-sm text-left leading-6 text-gray-600">
-              Already purchased? Find your documentation here
-            <SearchBar/>
+            <p className="text-sm text-left leading-6 text-gray-600">
+              Already purchased? Find your documentation here:
+            </p>
+            <div className="mt-4">
+              <SearchBar />
             </div>
           </div>
           <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
@@ -162,28 +134,33 @@ export default function Footer() {
                   ))}
                 </ul>
               </div>
-              {/* Contact section */}
-
               <div className="mt-10 md:mt-0">
                 <h3 className="text-sm font-semibold leading-6 text-ccDarkBlue">
                   Contact
                 </h3>
-                <Airtable tableName="Contact" view="Grid view" renderItem={contact} />
+                <Airtable
+                  tableName="Contact"
+                  view="Grid view"
+                  renderItem={ContactInfo}
+                />
               </div>
-              {/* Partner section */}
             </div>
             <div className="pl-8">
               <h3 className="text-sm font-semibold leading-6 text-ccDarkBlue">
                 Partners
               </h3>
               <div className="md:grid md:grid-cols-2">
-                <Airtable tableName="Partners" view="Grid view" renderItem={Partners} />
+                <Airtable
+                  tableName="Partners"
+                  view="Grid view"
+                  renderItem={Partners}
+                />
               </div>
             </div>
           </div>
         </div>
         <div className="mt-16 border-t border-ccLightBlue/10 pt-8 sm:mt-20 lg:mt-24">
-          <p className="text-xs leading-5 text-grey-400">
+          <p className="text-xs leading-5 text-gray-400">
             &copy; 2024 CC Diagnostics
           </p>
         </div>
